a95ce0f956133c03d630f457c4b62dfa213862b7
myun11 <yun.michael.ds@gmail.com>
2024-07-06 05:00:05 -0400

    Fixed vite config to deploy app

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   plugins: [react()],
+  base: "https://myun11.github.io/genshin-impact-web-app/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
