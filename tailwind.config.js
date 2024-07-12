135e30a83789c86122fe2325fd28609e40013266
myun11 <yun.michael.ds@gmail.com>
2024-07-11 21:48:09 -0400

    Last commit before laptop

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'paimon': "url('./src/images/transparent_paimon.png')",
+        'paimon': "url('/public/transparent_paimon.png')",
       }
     }
   },
