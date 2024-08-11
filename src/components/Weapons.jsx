00bed076f76434bbbeb675295f8114f8a9264cb4
myun11 <yun.michael.ds@gmail.com>
2024-08-11 03:08:59 -0400

    Fixed component update cycle with weapon api

--- src/components/Weapons.jsx
+++ src/components/Weapons.jsx
@@ -10,25 +10,43 @@ const Weapons = () => {
         try{
             await axios.get('https://genshin.jmp.blue/weapons')
             .then(res => {
-                const array = []
                 res.data.map(async weapon => {
                     if (weapon != "sword-of-narzissenkreuz") { // Until API is updated.
                         try {
                             await axios.get('https://genshin.jmp.blue/weapons/' + weapon)
                             .then(res2 => {
-                                // console.log("testing :",  res2.data)
-                                array.push({
+
+                                // This method updates state better than the method below.
+                                setWeapons([...weapons, {
                                     'id' : weapon,
                                     'data' : res2.data,
                                     'icon' : 'https://genshin.jmp.blue/weapons/' + weapon + '/icon'
-                                })
+                                }])
                             })
                         } catch (error) {
                             console.log("Error getting weapon data: ", error)
                         }
                     }
                 })
-                setWeapons(array)
+                // const array = []
+                // res.data.map(async weapon => {
+                //     if (weapon != "sword-of-narzissenkreuz") { // Until API is updated.
+                //         try {
+                //             await axios.get('https://genshin.jmp.blue/weapons/' + weapon)
+                //             .then(res2 => {
+                //                 // console.log("testing :",  res2.data)
+                //                 array.push({
+                //                     'id' : weapon,
+                //                     'data' : res2.data,
+                //                     'icon' : 'https://genshin.jmp.blue/weapons/' + weapon + '/icon'
+                //                 })
+                //             })
+                //         } catch (error) {
+                //             console.log("Error getting weapon data: ", error)
+                //         }
+                //     }
+                // })
+                // setWeapons(array)
             })
             .then(() => setLoading(false))
           } catch (error) {
@@ -49,9 +67,9 @@ const Weapons = () => {
                 <div className = "grid sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-8 gap-4 ">
                     {weapons.map(entry => {
                         return(
-                            <div className = "border-4 w-full h-full box rounded-lg">
-                                <button className = "" >
-                                    <img className="rounded-lg " src = {entry.icon} onClick = {() => {
+                            <div className = "border-4 w-full h-full box rounded-lg hover:bg-purple-500 hover:border-purple-500 transition duration-300 ease-in-out">
+                                <button className = "bg-gradient-to-b from-purple-500 to-white" >
+                                    <img className="w-full h-full rounded-lg " src = {entry.icon} onClick = {() => {
                                 
                                 }}/></button>
                                 <h2 className = "capitalize dark:text-white">{entry.data.name}</h2>
@@ -60,12 +78,7 @@ const Weapons = () => {
                         
                     })}                
                 </div>
-
-            
-            weapons length: {weapons.length}
-            <button onClick = {() => console.log(weapons.length)}>weps</button>
-            <img className="rounded-lg " src = "https://genshin.jmp.blue/weapons/thrilling-tales-of-dragon-slayers/icon"/>
-        </div>
+            </div>
         }
     </div>    
     )
