5be86e603719022ca5f5ce1f2dc9a5e576d06ba2
myun11 <yun.michael.ds@gmail.com>
2024-08-21 23:13:06 -0400

    Started mobile view for navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,25 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='dark:text-white bg-paimon bg-center w-screen bg-cover bg-slate-200 dark:bg-slate-800 bg-no-repeat min-h-screen'>
+    <div className='align-middle justify-center  dark:text-white bg-paimon bg-center w-screen bg-cover bg-slate-200 dark:bg-slate-800 bg-no-repeat min-h-screen'>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <h1>Welcome to Sumeru.gg</h1>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <h2>This web application serves as a data hub for users to plan and compare between various assets in the online game of Genshin Impact.</h2>
-        <div>
-          <h2>Skills Used:</h2>
-          <li>ReactJS</li>
-          <li>TailwindCSS</li>
-          <li>RESTful APIs</li>
-          <li>Responsive Design</li>
-          <li>Regex</li>
+        <br/><br/><br/>
+        
+        <div className="bg-slate-900">
+          <p className="text-2xl">
+          This web application serves as a data hub for users to plan and compare between various assets in the online game of Genshin Impact. 
+          Sumeru.gg provides data on character profiles, weapons, items and more. Everything you need in one place!
+          </p>
+          
+          <div>
+            <h2>Skills Used:</h2>
+            <li>ReactJS</li>
+            <li>TailwindCSS</li>
+            <li>RESTful APIs</li>
+            <li>Responsive Design</li>
+            <li>Regex</li>
+          </div>
         </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,93 @@ const Navbar = (props) => {
         // <button onClick = {() => props.setPage('Dev')} className="text-gray-300 hover:text-white">Dev</button>
         <div className="">
             <nav className="bg-slate-700 p-4 text-left fixed w-screen z-10 top-0">
-            <div className="">
+                {/* Mobile */}
+                <div className="md:hidden">
+                    <button
+                        onClick={() => setIsOpen(!isOpen)}
+                        className="text-gray-300 focus:outline-none"
+                    >
+                        <svg
+                        className="w-6 h-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        >
+                        {isOpen ? (
+                            <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12"
+                            />
+                        ) : (
+                            <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4 6h16M4 12h16m-7 6h7"
+                            />
+                        )}
+                        </svg>
+                    </button>
+                </div>
+                {isOpen && (
+                    <div className="md:hidden">
+                    <a
+                        href="#"
+                        className="block text-gray-300 hover:text-white py-2"
+                        onClick={() => {
+                            setIsOpen(false)
+                            props.setPage('Home')
+                        }}
+                    >
+                        Home
+                    </a>
+                    <a
+                        href="#"
+                        className="block text-gray-300 hover:text-white py-2"
+                        onClick={() => {
+                            setIsOpen(false)
+                            props.setPage('Characters')
+                        }}
+                    >
+                        Characters
+                    </a>
+                    <a
+                        href="#"
+                        className="block text-gray-300 hover:text-white py-2"
+                        onClick={() => {
+                            setIsOpen(false)
+                            props.setPage('Weapons')
+                        }}
+                    >
+                        Weapons
+                    </a>
+                    <a
+                        href="#"
+                        className="block text-gray-300 hover:text-white py-2"
+                        onClick={() => {
+                            setIsOpen(false)
+                            props.setPage('Artifacts')
+                        }}
+                    >
+                        Artifacts
+                    </a>
+                    <a
+                        href="#"
+                        className="block text-gray-300 hover:text-white py-2"
+                        onClick={() => {
+                            setIsOpen(false)
+                            props.setPage('Consumables')
+                        }}
+                    >
+                        Consumables
+                    </a>
+                    </div>
+                )}
+
+                {/* PC */}
                 <div className="space-x-10 inline-flex align-middle m-auto mx-auto">
                     {/* Web App Icon */}
                     <div onClick = {() => props.setPage('Home')} className="inline-flex align-middle text-white text-lg font-bold">
@@ -29,72 +115,8 @@ const Navbar = (props) => {
                     <a href='#' onClick = {() => props.setPage('Consumables')} className="font-bold m-auto text-3xl text-gray-300 hover:text-white">Consumables</a>
                     <Theme/>
                 </div>
-                <div className="md:hidden">
-                <button
-                    onClick={() => setIsOpen(!isOpen)}
-                    className="text-gray-300 focus:outline-none"
-                >
-                    <svg
-                    className="w-6 h-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    >
-                    {isOpen ? (
-                        <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M6 18L18 6M6 6l12 12"
-                        />
-                    ) : (
-                        <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h16m-7 6h7"
-                        />
-                    )}
-                    </svg>
-                </button>
-                </div>
-            </div>
-
-        {isOpen && (
-            <div className="md:hidden">
-            <a
-                href="#home"
-                className="block text-gray-300 hover:text-white py-2"
-                onClick={() => setIsOpen(false)}
-            >
-                Home
-            </a>
-            <a
-                href="#about"
-                className="block text-gray-300 hover:text-white py-2"
-                onClick={() => setIsOpen(false)}
-            >
-                About
-            </a>
-            <a
-                href="#services"
-                className="block text-gray-300 hover:text-white py-2"
-                onClick={() => setIsOpen(false)}
-            >
-                Services
-            </a>
-            <a
-                href="#contact"
-                className="block text-gray-300 hover:text-white py-2"
-                onClick={() => setIsOpen(false)}
-            >
-                Contact
-            </a>
-            </div>
-        )}
-        </nav>
-    </div>
+            </nav>
+        </div>
     )
 }
 
