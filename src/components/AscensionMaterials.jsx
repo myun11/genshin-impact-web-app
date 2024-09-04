47541538fa59407b7ae7a19799f33078490e9fef
myun11 <yun.michael.ds@gmail.com>
2024-09-03 22:47:44 -0400

    Added styling for ascension materials

--- src/components/AscensionMaterials.jsx
+++ src/components/AscensionMaterials.jsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react'
-
+import axios from 'axios'
+import mora from '../images/mora.webp'
 const AscensionMaterials = (props) => {
     const levels = ["level_20", "level_40", "level_50", "level_60", "level_70", "level_80"]
     const [selectedTab, setSelectedTab] = useState("Total")
     const [totalMaterials, setTotalMaterials] = useState([])
 
+    // All different types of materials. This is used for differentiating the endpoint because the images are categorized by the item's type.
+    const [commonMaterials, setCommonMaterials] = useState([])
+    const [localSpecialties, setLocalSpecialties] = useState([])
+    const [gemMaterials, setGemMaterials] = useState([])
+    const [bossMaterials, setBossMaterials] = useState([])
+
     // Used for taking the total materials from all ascension levels
     const sumMaterials = (materials) => {
-        const reduced = Object.values(materials).flat().reduce((acc, item) => {
+        const reduced = Object.values(materials).flat().sort((a,b) => a["name"] > b["name"]).reduce((acc, item) => {
           if (acc[item.name]) {
             acc[item.name] += item.value;
           } else {
@@ -22,37 +29,56 @@ const AscensionMaterials = (props) => {
 
     // Used for making the API endpoint for each item image.
     const itemImage = (item) => {
+        if (item == 'Mora') {
+            return mora
+        }
         try {
             const itemName = item.toLowerCase().replaceAll(' ', '-').replaceAll("'", '-')
             let endpoint = ""
-            if (itemName.includes("vayuda") ||
-                itemName.includes("shivada") ||
-                itemName.includes("vajrada") ||
-                itemName.includes("prithiva") ||
-                itemName.includes("varunada") ||
-                itemName.includes("agnidus") ||
-                itemName.includes("nagadus") ||
-                itemName.includes("brilliant")) {
+            if (gemMaterials.includes(itemName)) {
                 endpoint = 'https://genshin.jmp.blue/materials/character-ascension/' + itemName
-            } else if (itemName.includes == "") {
+            } else if (localSpecialties.includes(itemName)) {
                 endpoint = 'https://genshin.jmp.blue/materials/local-specialties/' + itemName
-            } else {
+            } else if (bossMaterials.includes(itemName)) {
+                endpoint = 'https://genshin.jmp.blue/materials/boss-material/' + itemName
+            } else if (commonMaterials.includes(itemName)) {
                 endpoint = 'https://genshin.jmp.blue/materials/common-ascension/' + itemName
             }
-            console.log("endpoint ", endpoint)
             return endpoint
         } catch (error) {
             console.log("Error: ", error)
         }
     }
+
+    async function fetchLists() {
+        try {
+            await axios.all([
+                axios.get('https://genshin.jmp.blue/materials/character-ascension/list'),
+                axios.get('https://genshin.jmp.blue/materials/common-ascension/list'),
+                axios.get('https://genshin.jmp.blue/materials/local-specialties/list'),
+                axios.get('https://genshin.jmp.blue/materials/boss-material/list')
+            ])
+            .then(axios.spread((gems, common, local, boss) => {
+                setGemMaterials(gems.data)
+                setCommonMaterials(common.data)
+                setLocalSpecialties(local.data)
+                setBossMaterials(boss.data)
+            }))
+        } catch (error) {
+            console.log("Error fetching item list data: ", error)
+        }
+    }
     useEffect(() => {
+        // Loads the item types
+        fetchLists()
+        // Takes the sum of materials
         setTotalMaterials(sumMaterials(props.data))
     }, [])
     return (
         <div>
             <h1 className = "font-bold p-4">Ascension Materials</h1>
-            <button onClick = {() => console.log(totalMaterials)}>total mats</button>
             <div class="md:flex">
+                {/* Tabs */}
                 <ul class="flex-column space-y space-y-4 text-xl w-1/4 font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
                     <li>
                         <button class={selectedTab == "Total" ?
@@ -79,30 +105,32 @@ const AscensionMaterials = (props) => {
                         )
                     })}
                 </ul>
+                {/* Body */}
                 <div class="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full">
                     {   selectedTab == "Total" ?  
-                            <div>
+                            <div className = "inline-flex">
                                 {totalMaterials.map(entry => {
                                     return(
                                         <div>
                                             <img src = {itemImage(entry["name"])}/>
-                                            Material: {entry["name"]}
-                                            Amount: {entry["value"]}
+                                            <p> {entry["value"] + " " + entry["name"]}</p>
+                                            {/* <p>Material: {entry["name"]}</p>
+                                            <p>Amount: {entry["value"]}</p> */}
                                         </div>
                                     )
                                 })}
                             </div> :
-                            <div>
+                            <div >
                                 {levels.map((entry, idx) => {
                                     return(
-                                        <div>
+                                        <div className = "inline-flex">
                                             {props.data[entry].map(lvl => {
                                                 <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2">Ascension {idx + 1} Materials</h3>
                                                 if (selectedTab == idx) {
                                                     return(
                                                         <div>
-                                                            Material: {lvl["name"]}
-                                                            Amount: {lvl["value"]}
+                                                            <img src = {itemImage(lvl["name"])}/>
+                                                            <p> {lvl["value"] + " " + lvl["name"]}</p>
                                                         </div>
                                                     )
                                                 }
@@ -114,9 +142,6 @@ const AscensionMaterials = (props) => {
                     }
                 </div>
             </div>
-
-
-            <button onClick = {() => console.log(props.data)}>data</button>
         </div>
     )
 }
