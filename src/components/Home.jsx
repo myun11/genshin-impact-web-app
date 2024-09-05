41bf3da61c8ebc52334c3cba674c53756bd2cd9c
myun11 <yun.michael.ds@gmail.com>
2024-09-05 12:53:57 -0400

    Added light mode styling for home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = (props) => {
       <br/><br/><br/><br/>
       <div className="">
         <div className="md:w-3/5 p-4 mx-auto bg-slate-300 dark:bg-slate-800 bg-opacity-50 space-y-5">
-          <h1 className="text-8xl">Welcome to Sumeru.gg</h1>
+          <h1 className="md:text-8xl">Welcome to Sumeru.gg</h1>
           <p className="text-md lg:text-2xl">
             This web application serves as a data hub for users to plan and compare between various assets in the online game of Genshin Impact. 
             Sumeru.gg provides data on character profiles, weapons, items and more. Everything you need in one place!
@@ -14,7 +14,7 @@ const Home = (props) => {
         </div>
         <div className="md:w-3/5 p-4 mx-auto">
           <div className="grid grid-cols-2 p-2 my-4 gap-5">
-            <button className="group relative transition lg:h-52 h-20 rounded-lg bg-slate-500" onClick = {() => props.setPage("Characters")}>
+            <button className="group relative transition lg:h-52 h-20 rounded-lg border-2 border-black dark:border-slate-800 bg-slate-100 dark:bg-slate-500" onClick = {() => props.setPage("Characters")}>
               <h2 className="md:text-2xl lg:text-4xl">Characters</h2>
               <p className="p-4 max-md:hidden">
                 An assortment of characters with filter tools for querying specific needs.
@@ -22,7 +22,7 @@ const Home = (props) => {
               </p>
               <div className="absolute inset-0 h-full w-0 bg-white/30 transition-[width] group-hover:w-full"></div>
             </button>
-            <button className="group relative transition lg:h-52 h-20 rounded-lg bg-slate-500" onClick = {() => props.setPage("Weapons")}>
+            <button className="group relative transition lg:h-52 h-20 rounded-lg border-2 border-black dark:border-slate-800 bg-slate-100 dark:bg-slate-500" onClick = {() => props.setPage("Weapons")}>
               <h2 className="md:text-2xl lg:text-4xl">Weapons</h2>
               <p className="p-4 max-md:hidden">
                 A grid of weapons with filter tools to find your ideal weapon.
@@ -30,7 +30,7 @@ const Home = (props) => {
               </p>
               <div className="absolute inset-0 h-full w-0 bg-white/30 transition-[width] group-hover:w-full"></div>              
             </button>
-            <button className="group relative transition lg:h-52 h-20 rounded-lg bg-slate-500" onClick = {() => props.setPage("Artifacts")}>
+            <button className="group relative transition lg:h-52 h-20 rounded-lg border-2 border-black dark:border-slate-800 bg-slate-100 dark:bg-slate-500" onClick = {() => props.setPage("Artifacts")}>
               <h2 className="md:text-2xl lg:text-4xl">Artifacts</h2>
               <p className="p-4 max-md:hidden">
                 A table of different artifacts organized by their class set.
@@ -38,7 +38,7 @@ const Home = (props) => {
               </p>
               <div className="absolute inset-0 h-full w-0 bg-white/30 transition-[width] group-hover:w-full"></div> 
             </button>
-            <button className="group relative transition lg:h-52 h-20 rounded-lg bg-slate-500" onClick = {() => props.setPage("Consumables")}>
+            <button className="group relative transition lg:h-52 h-20 rounded-lg border-2 border-black dark:border-slate-800 bg-slate-100 dark:bg-slate-500" onClick = {() => props.setPage("Consumables")}>
               <h2 className="md:text-2xl lg:text-4xl">Consumables</h2>
               <p className="p-4 max-md:hidden">
                 A collection of different consumables and their buffs.
