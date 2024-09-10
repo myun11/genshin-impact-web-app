a69bbebbe1a08e82a2a691c742a72d74527c12df
myun11 <yun.michael.ds@gmail.com>
2024-09-10 02:09:46 -0400

    Started radar chart for normal attack

--- src/components/RadarChart.jsx
+++ src/components/RadarChart.jsx
@@ -4,24 +4,9 @@ const RadarChart = (props) => {
     const [data, setData] = useState([])
     const [aggregate, setAggregate] = useState([])
 
-    // Most common are
-
-    // BOTH
-    // 1-6 hit dmg,
-
-    // MELEE
-    // Charged Attack DMG
-    // Charged Attack Stamina Cost
-
-    // RANGED
-    // Aimed Shot || Aimed Shot DMG || Level 1 Aimed Shot DMG
-    // Fully-Charged Aimed Shot || Fully-Charged Aimed Shot DMG || Aimed Shot Charge Level 1 DMG 
-
-    // BOTH
-    // Plunge DMG
-    // Low/High Plunge DMG || Low / High Plunge DMG
-
-    const calculate = () => {
+    // This function returns the frequency of all types of skill data.
+    // This is helpful because there are a lot of misnamed skill descriptors and typos that I count as the same skill.
+    const getAverageCounts = () => {
         let hashmap = {
             "error" : 0
         }
@@ -39,20 +24,113 @@ const RadarChart = (props) => {
             }
             
         })
-        console.log(hashmap)
+        return hashmap
+    }
+
+    // Helper function that returns the combined descriptor for multiple misspelled skill descriptors
+    // Most common are
+
+    // BOTH
+    // 1-6 hit dmg,
+
+    // MELEE
+    // Charged Attack DMG
+    // Charged Attack Stamina Cost
+
+    // RANGED
+    // Aimed Shot || Aimed Shot DMG || Level 1 Aimed Shot DMG
+    // Fully-Charged Aimed Shot || Fully-Charged Aimed Shot DMG || Aimed Shot Charge Level 1 DMG 
+
+    // BOTH
+    // Plunge DMG
+    // Low/High Plunge DMG || Low / High Plunge DMG
+    const getFinalName = (name) => {
+        if (name == "Aimed Shot" || name == "Aimed Shot DMG" || name == "Level 1 Aimed Shot DMG") {
+            return "Aimed Shot DMG"
+        }
+        if (name == "Fully-Charged Aimed Shot" || name == "Fully-Charged Aimed Shot DMG" || name == "Aimed Shot Charge Level 1 DMG") {
+            return "Fully Charged Aimed Shot DMG"
+        }
+        if (name == "Low/High Plunge DMG" || name == "Low / High Plunge DMG") {
+            return "Low / High Plunge DMG"
+        }
+        return name
+    }
+
+    // Get all average values of skill descriptors
+    const getAverageValues = () => {
+        const names = {}
+        props.rosterData.map(character => {
+            try {
+                character.skillTalents[0]?.upgrades.map(skill => {
+                    let finalName = getFinalName(skill.name)
+                    if (finalName.includes('/')) {
+                        // For Low / High Plunge DMG
+                        let val = skill.value.replace('%','').split('/')
+
+                        if (Object.keys(names).includes(finalName)) {
+                            // If the skill descriptor already exists in hashmap
+                            names[finalName].count += 1
+                            names[finalName].value.low += eval(val[0])
+                            names[finalName].value.high += eval(val[1])
+                        } else { 
+                            // New skill descriptor
+                            
+                            names[finalName] = {
+                                "count" : 1,
+                                "value" : {
+                                    "low" : eval(val[0]),
+                                    "high" : eval(val[1])
+                                }
+                            }                        
+                        }
+                    } else {
+                        let val = skill.value.replace('%','')
+                        
+                        // For values with multiple numbers e.g a sum of numbers, we add them together. Except for division                    
+                        val = eval(val)
+
+                        if (Object.keys(names).includes(finalName)) {
+                            // If the skill descriptor already exists in hashmap
+                            names[finalName].count += 1
+                            names[finalName].value += val
+                        } else { 
+                            // New skill descriptor
+                            
+                            names[finalName] = {
+                                "count" : 1,
+                                "value" : val
+                            }                        
+                        }
+                    }
+                })
+            } catch (error) {
+                // console.log("Error in hash ")
+            }
+        })
+
+        Object.keys(names).map(entry => {
+            names[entry] = names[entry].value / names[entry].count
+        })
+
+        return names
+    }
+
+    // Gets all skill type descriptor names
+    const getNames = () => {
+
     }
     useEffect(() => {
-        calculate()
+        getAverageCounts()
     }, [])
     return (
         <div>
             <button onClick = {() => console.log(props.rosterData)}>all</button>
             <button onClick = {() => console.log(props.charPreviewData.skillTalents[0].upgrades)}>this char</button>
             <button onClick = {() => console.log(aggregate)}>aggregate</button>
-            <button onClick = {() => {
-                calculate()
-            }}>Calculate Averages</button>
-            <button onClick = {() => console.log(eval("55.25% + 55.25%"))}>eval</button>
+            <button onClick = {() => console.log(getAverageCounts())}>getAverageCount</button>
+            <button onClick = {() => console.log(getAverageValues())}>getAverageValues</button>
+            <button onClick = {() => console.log("aaaaa".split('+'))}>test</button>
             {/* <button onClick = {() => props.showChart(prev => !prev)}>Toggle Table/Chart</button> */}
             Begin radar chart.
         </div>
