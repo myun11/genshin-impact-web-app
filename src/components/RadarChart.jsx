4077cf5603d36cbd871b6813ac44f1d908a8bed4
myun11 <yun.michael.ds@gmail.com>
2024-09-10 23:01:07 -0400

    Added highcharts

--- src/components/RadarChart.jsx
+++ src/components/RadarChart.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import Highcharts from 'highcharts'
-import HighchartsReact from 'highcharts-react-official'
+import Highcharts from "highcharts/highstock";
+import HighchartsReact from "highcharts-react-official";
+import HC_more from "highcharts/highcharts-more";
 
+// init the module
+HC_more(Highcharts);
 const RadarChart = (props) => {
     const [aggregate, setAggregate] = useState({
         "1-Hit DMG": 51.52434782608696,
@@ -268,69 +271,83 @@ const RadarChart = (props) => {
                  
                 <div className="">
                     <HighchartsReact
-                        highcharts={totalData}
+                        highcharts={Highcharts}
+                        constructor='chart'
+                        allowChartUpdate={true}
                         options={{
                             chart: {
-                                polar: true
+                                polar: true,
+                                type: 'line'
                             },
-                        
+                            // subtitle: {
+                            //     text: ''
+                            // },
                             title: {
-                                text: 'Highcharts Polar Chart'
-                            },
-                        
-                            subtitle: {
-                                text: 'Also known as Radar Chart'
+                                text: 'Budget vs spending',
+                                x: -80
                             },
                         
                             pane: {
-                                startAngle: 0,
-                                endAngle: 360
+                                size: '80%'
                             },
                         
                             xAxis: {
-                                tickInterval: 45,
-                                min: 0,
-                                max: 360,
-                                labels: {
-                                    format: '{value}°'
-                                }
+                                categories: [
+                                    'Sales', 'Marketing', 'Development', 'Customer Support',
+                                    'Information Technology', 'Administration'
+                                ],
+                                tickmarkPlacement: 'on',
+                                lineWidth: 0
                             },
                         
                             yAxis: {
+                                gridLineInterpolation: 'polygon',
+                                lineWidth: 0,
                                 min: 0
                             },
                         
-                            plotOptions: {
-                                series: {
-                                    pointStart: 0,
-                                    pointInterval: 45
-                                },
-                                column: {
-                                    pointPadding: 0,
-                                    groupPadding: 0
-                                }
+                            tooltip: {
+                                shared: true,
+                                pointFormat: '<span style="color:{series.color}">{series.name}: <b>' +
+                                    '${point.y:,.0f}</b><br/>'
+                            },
+                        
+                            legend: {
+                                align: 'right',
+                                verticalAlign: 'middle',
+                                layout: 'vertical'
                             },
                         
                             series: [{
-                                type: 'column',
-                                name: 'Column',
-                                data: [8, 7, 6, 5, 4, 3, 2, 1],
-                                pointPlacement: 'between'
+                                name: 'Allocated Budget',
+                                data: [43000, 19000, 60000, 35000, 17000, 10000],
+                                pointPlacement: 'on'
                             }, {
-                                type: 'line',
-                                name: 'Line',
-                                data: [1, 2, 3, 4, 5, 6, 7, 8]
-                            }, {
-                                type: 'area',
-                                name: 'Area',
-                                data: [1, 8, 2, 7, 3, 6, 4, 5]
-                            }]
+                                name: 'Actual Spending',
+                                data: [50000, 39000, 42000, 31000, 26000, 14000],
+                                pointPlacement: 'on'
+                            }],
+                        
+                            responsive: {
+                                rules: [{
+                                    condition: {
+                                        maxWidth: 500
+                                    },
+                                    chartOptions: {
+                                        legend: {
+                                            align: 'center',
+                                            verticalAlign: 'bottom',
+                                            layout: 'horizontal'
+                                        },
+                                        pane: {
+                                            size: '70%'
+                                        }
+                                    }
+                                }]
+                            }
                         }}
                     />
-                </div>
-
-
-            
+                </div>           
         </div>
   )
 }
