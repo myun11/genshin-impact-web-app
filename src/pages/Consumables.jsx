76f157ac4bb049d72fbc932953eb78dad278c3ca
myun11 <yun.michael.ds@gmail.com>
2024-09-12 22:51:11 -0400

    Consumables page empty

--- src/pages/Consumables.jsx
+++ src/pages/Consumables.jsx
@@ -18,7 +18,7 @@ const Consumables = () => {
     <div className="p-2 md:p-4 w-5/6 mx-auto min-h-screen">
       <div className="h-20"></div>
       <h1 className="p-4 text-black dark:text-white">Consumables</h1>
-      <button onClick = {() => console.log(fetchData('food'))}>Fetch Data</button>
+      {/* <button onClick = {() => console.log(fetchData('food'))}>Fetch Data</button> */}
       
     </div>
   )
