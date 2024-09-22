8b2a960fb3a2989f94303678912daed288a4b29e
Michael Yun <yun.michael.ds@gmail.com>
2024-09-21 23:19:44 -0400

    Started fixing styling choices for laptop view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ const Home = (props) => {
       <div className="absolute inset-0 z-0 h-full w-full px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#00a_100%)] dark:[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#00a_100%)]"></div>
         <div className = "absolute top-48 h-full min-h-screen">
           <div className="md:w-4/5 p-4 mx-auto bg-slate-300 dark:bg-slate-800 dark:bg-opacity-50 bg-opacity-50 space-y-5 rounded-3xl">
-            <h1 className="md:text-8xl font-extrabold">Welcome to Sumeru.dev</h1>
-            <p className="text-md lg:text-2xl lg:w-1/2 lg:flex-col item-center justify-center mx-auto">
+            <h1 className="md:text-5xl font-extrabold">Welcome to Sumeru.dev</h1>
+            <p className="text-base lg:w-1/2 lg:flex-col item-center justify-center mx-auto">
               This web application serves as a data hub for users to plan and compare between various assets in the online game of Genshin Impact.
               Sumeru.dev provides data on character profiles, weapons, items and more. Everything you need in one place!
             </p>
@@ -21,7 +21,7 @@ const Home = (props) => {
                 window.scrollTo(0, 0)
               }}>
                 <h2 className="md:text-2xl lg:text-4xl md:font-bold">Characters</h2>
-                <p className="p-4 md:text-sm lg:text-xl max-md:hidden">
+                <p className="p-4 md:text-sm lg:text-base max-md:hidden">
                   An assortment of characters with filter tools for querying specific needs.
                   Select your desired character to view detailed information including their skills, ascension materials, and more.
                 </p>
@@ -32,7 +32,7 @@ const Home = (props) => {
                 window.scrollTo(0, 0)
               }}>
                 <h2 className="md:text-2xl lg:text-4xl md:font-bold">Weapons</h2>
-                <p className="p-4 md:text-sm lg:text-xl max-md:hidden">
+                <p className="p-4 md:text-sm lg:text-base max-md:hidden">
                   A grid of weapons with filter tools to find your ideal weapon.
                   Select your desired weapon to view detailed information including rarity and location of the weapon.
                 </p>
@@ -43,7 +43,7 @@ const Home = (props) => {
                 window.scrollTo(0, 0)
               }}>
                 <h2 className="md:text-2xl lg:text-4xl md:font-bold">Artifacts</h2>
-                <p className="p-4 md:text-sm lg:text-xl max-md:hidden">
+                <p className="p-4 md:text-sm lg:text-base max-md:hidden">
                   A table of different artifacts organized by their class set.
                   View set bonuses here.
                 </p>
@@ -54,7 +54,7 @@ const Home = (props) => {
                 window.scrollTo(0, 0)
               }}>
                 <h2 className="md:text-2xl lg:text-4xl md:font-bold">Consumables</h2>
-                <p className="p-4 md:text-sm lg:text-xl max-md:hidden">
+                <p className="p-4 md:text-sm lg:text-base max-md:hidden">
                   A collection of different consumables and their buffs.
                 </p>
                 <div className="absolute inset-0 h-full w-0 bg-gray-600 bg-opacity-30 dark:bg-white/30 transition-[width] group-hover:w-full"></div> 
